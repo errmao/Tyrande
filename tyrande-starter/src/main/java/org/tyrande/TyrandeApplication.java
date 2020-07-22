c91cc9c442864656a585930318f12ee52bb3bca0
zzw <errmao@aliyun.com>
2020-07-22 15:29:40 +0800

    Initial commit

---
+++ tyrande-starter/src/main/java/org/tyrande/TyrandeApplication.java
@@ -0,0 +1,17 @@
+package org.tyrande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Tyrande
+ */
+@SpringBootApplication
+public class TyrandeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TyrandeApplication.class, args);
+    }
+}
---
+++ tyrande-system/src/main/java/org/tyrande/TestController.java
@@ -0,0 +1,20 @@
+package org.tyrande;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * s
+ *
+ * @author Tyrande
+ */
+@RestController
+@RequestMapping(("test"))
+public class TestController {
+
+    @GetMapping("ttt2")
+    public String gettt() {
+        return "sssssss";
+    }
+}
