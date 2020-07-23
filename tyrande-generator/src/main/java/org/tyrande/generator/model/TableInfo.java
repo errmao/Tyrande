813d33188f1922b7aa52ca9385a142209ffe6c58
zzw <errmao@aliyun.com>
2020-07-23 17:08:42 +0800

    代码生成器开发-进度-查询到表和字段的信息

--- tyrande-generator/src/main/java/org/tyrande/generator/model/TableInfo.java
+++ tyrande-generator/src/main/java/org/tyrande/generator/model/TableInfo.java
@@ -10,6 +10,12 @@ import lombok.Data;
 @Data
 public class TableInfo {
 
+    /**
+     * 表名
+     */
     private String tableName;
+    /**
+     * 表注释
+     */
     private String tableComment;
 }
