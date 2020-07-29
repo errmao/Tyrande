f8c4800155d1a4887a9bf8d9b56d4bd993955821
zzw <errmao@aliyun.com>
2020-07-29 13:16:02 +0800

    Spring security 登录优化

--- tyrande-security/src/main/java/org/tyrande/security/security/JwtLoginFilter.java
+++ tyrande-security/src/main/java/org/tyrande/security/security/JwtLoginFilter.java
@@ -1,12 +1,15 @@
 package org.tyrande.security.security;
 
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.tyrande.common.utils.FrameworkUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,16 +25,30 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class JwtLoginFilter extends AbstractAuthenticationProcessingFilter {
 
+    public static final String SPRING_SECURITY_FORM_USERNAME_KEY = "username";
+    public static final String SPRING_SECURITY_FORM_PASSWORD_KEY = "password";
+
     public JwtLoginFilter(String url) {
         super(new AntPathRequestMatcher(url, "POST"));
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String userName = "";
+        String password = "";
+        // Json 格式
+        if (request.getContentType().equals(MediaType.APPLICATION_JSON_UTF8_VALUE) ||
+            request.getContentType().equals(MediaType.APPLICATION_JSON_VALUE)) {
+            JSONObject paramsObj = FrameworkUtil.getRequestParamsObj(request);
+            userName = paramsObj.getString(SPRING_SECURITY_FORM_USERNAME_KEY);
+            password = paramsObj.getString(SPRING_SECURITY_FORM_PASSWORD_KEY);
+        } else {
+            // form-data 格式
+            userName = obtainUsername(request);
+            password = obtainPassword(request);
+        }
+        log.info("[用户登陆 : {}]", userName);
         try {
-            String userName = request.getParameter("username");
-            String password = request.getParameter("password");
-            log.info("[用户登陆 : {}]", userName);
             // 创建未认证的凭证(etAuthenticated(false)),注意此时凭证中的主体principal为用户名
             JwtLoginToken jwtLoginToken = new JwtLoginToken(userName, password);
             // 将认证详情(ip,sessionId)写到凭证
@@ -43,5 +60,13 @@ public class JwtLoginFilter extends AbstractAuthenticationProcessingFilter {
         } catch (Exception e) {
             throw new BadCredentialsException(e.getMessage());
         }
+    }
+
+    protected String obtainPassword(HttpServletRequest request) {
+        return request.getParameter(SPRING_SECURITY_FORM_PASSWORD_KEY);
+    }
+
+    protected String obtainUsername(HttpServletRequest request) {
+        return request.getParameter(SPRING_SECURITY_FORM_USERNAME_KEY);
     }
 }
