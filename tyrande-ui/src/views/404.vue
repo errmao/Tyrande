1a0b2a5eea31c8f341a047e4683c7a3f2773691a
zzw <errmao@aliyun.com>
2020-07-30 15:45:55 +0800

    主体区域样式 添加背景色灰

--- tyrande-ui/src/views/404.vue
+++ tyrande-ui/src/views/404.vue
@@ -10,7 +10,7 @@
       <div class="bullshit">
         <div class="bullshit__oops">OOPS!</div>
         <div class="bullshit__info">All rights reserved
-          <a style="color:#20a0ff" href="https://wallstreetcn.com" target="_blank">wallstreetcn</a>
+          <a style="color:#20a0ff" href="https://github.com/errmao/Tyrande" target="_blank">Tyrande</a>
         </div>
         <div class="bullshit__headline">{{ message }}</div>
         <div class="bullshit__info">Please check that the URL you entered is correct, or click the
