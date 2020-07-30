baa09c8b90717de9ba5091cda12bb6adab41035b
zzw <errmao@aliyun.com>
2020-07-30 15:59:53 +0800

    修改系统名称为后台管理系统

--- tyrande-ui/src/views/login/index.vue
+++ tyrande-ui/src/views/login/index.vue
@@ -4,7 +4,7 @@
              auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">泰兰德系统</h3>
+        <h3 class="title">欢迎登陆后台管理系统</h3>
       </div>
 
       <el-form-item prop="username">
