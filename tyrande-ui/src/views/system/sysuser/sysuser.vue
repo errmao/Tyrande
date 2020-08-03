637065e9a6cfcb4d8b5276a5a3a4fa8472f94300
zzw <errmao@aliyun.com>
2020-08-03 16:08:25 +0800

    用户管理优化

--- tyrande-ui/src/views/system/sysuser/sysuser.vue
+++ tyrande-ui/src/views/system/sysuser/sysuser.vue
@@ -3,8 +3,11 @@
     <!-- 查询条件 -->
     <el-card>
       <el-form :inline="true" :model="searchForm" class="app-container-searchForm">
-        <el-form-item label="主键">
-          <el-input v-model="searchForm.id" placeholder="主键"></el-input>
+        <el-form-item label="登录账户">
+          <el-input v-model="searchForm.loginCode" placeholder="登录账户"></el-input>
+        </el-form-item>
+        <el-form-item label="用户名称">
+          <el-input v-model="searchForm.userName" placeholder="用户名称"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" icon="el-icon-search" @click="getPageList">
@@ -26,19 +29,17 @@
       <!-- 数据表格 -->
       <el-table border :data="gridData.list">
         <el-table-column type="index" label="序号" width="50"/>
-
         <el-table-column prop="loginCode" label="登陆账户"/>
-        <el-table-column prop="password" label="密码"/>
         <el-table-column prop="userName" label="用户名称"/>
-        <el-table-column prop="sex" label="性别 1-男 2-女"/>
+        <el-table-column prop="sex" label="性别"/>
         <el-table-column prop="age" label="年龄"/>
         <el-table-column prop="avatar" label="头像"/>
         <el-table-column prop="address" label="地址"/>
-        <el-table-column prop="email" label="邮箱"/>
-        <el-table-column prop="validEmail" label="邮箱是否验证 0-否 1-是"/>
+        <el-table-column prop="email" label="邮箱" width="160"/>
+        <el-table-column prop="validEmail" label="邮箱验证"/>
         <el-table-column prop="tel" label="电话"/>
-        <el-table-column prop="validTel" label="电话是否验证 0-否 1-是"/>
-        <el-table-column prop="status" label="用户状态 0-未使用 1-正常 2-锁定 3-过期"/>
+        <el-table-column prop="validTel" label="电话验证"/>
+        <el-table-column prop="status" label="用户状态"/>
         <el-table-column fixed="right" label="操作" width="180">
           <template slot-scope="scope">
             <el-button type="text" icon="el-icon-view" size="small"
@@ -69,40 +70,37 @@
     <el-dialog :title="defaultSettings.btnView" :visible.sync="visibleConfig.view" width="50%">
           <span>
             <el-form :model="viewForm" label-width="120px" disabled>
-                 <el-form-item label="登陆账户" prop="loginCode">
+              <el-form-item label="登陆账户" prop="loginCode">
                 <el-input v-model="viewForm.loginCode"/>
               </el-form-item>
-                 <el-form-item label="密码" prop="password">
-                <el-input v-model="viewForm.password"/>
-              </el-form-item>
-                 <el-form-item label="用户名称" prop="userName">
+              <el-form-item label="用户名称" prop="userName">
                 <el-input v-model="viewForm.userName"/>
               </el-form-item>
-                 <el-form-item label="性别 1-男 2-女" prop="sex">
+              <el-form-item label="性别" prop="sex">
                 <el-input v-model="viewForm.sex"/>
               </el-form-item>
-                 <el-form-item label="年龄" prop="age">
+              <el-form-item label="年龄" prop="age">
                 <el-input v-model="viewForm.age"/>
               </el-form-item>
-                 <el-form-item label="头像" prop="avatar">
+              <el-form-item label="头像" prop="avatar">
                 <el-input v-model="viewForm.avatar"/>
               </el-form-item>
-                 <el-form-item label="地址" prop="address">
+              <el-form-item label="地址" prop="address">
                 <el-input v-model="viewForm.address"/>
               </el-form-item>
-                 <el-form-item label="邮箱" prop="email">
+              <el-form-item label="邮箱" prop="email">
                 <el-input v-model="viewForm.email"/>
               </el-form-item>
-                 <el-form-item label="邮箱是否验证 0-否 1-是" prop="validEmail">
+              <el-form-item label="邮箱验证" prop="validEmail">
                 <el-input v-model="viewForm.validEmail"/>
               </el-form-item>
-                 <el-form-item label="电话" prop="tel">
+              <el-form-item label="电话" prop="tel">
                 <el-input v-model="viewForm.tel"/>
               </el-form-item>
-                 <el-form-item label="电话是否验证 0-否 1-是" prop="validTel">
+              <el-form-item label="电话验证" prop="validTel">
                 <el-input v-model="viewForm.validTel"/>
               </el-form-item>
-                 <el-form-item label="用户状态 0-未使用 1-正常 2-锁定 3-过期" prop="status">
+              <el-form-item label="用户状态" prop="status">
                 <el-input v-model="viewForm.status"/>
               </el-form-item>
             </el-form>
@@ -116,52 +114,52 @@
     <!-- 添加 -->
     <el-dialog :title="defaultSettings.btnAdd" :visible.sync="visibleConfig.add" width="50%"
                @close="doAddClose">
-          <span>
-            <el-form :model="addForm" :rules="checkRules" ref="addFormRef" label-width="120px">
-                 <el-form-item label="登陆账户" prop="loginCode">
-                <el-input v-model="addForm.loginCode"></el-input>
-              </el-form-item>
-                 <el-form-item label="密码" prop="password">
-                <el-input v-model="addForm.password"></el-input>
-              </el-form-item>
-                 <el-form-item label="用户名称" prop="userName">
-                <el-input v-model="addForm.userName"></el-input>
-              </el-form-item>
-                 <el-form-item label="性别 1-男 2-女" prop="sex">
-                <el-input v-model="addForm.sex"></el-input>
-              </el-form-item>
-                 <el-form-item label="年龄" prop="age">
-                <el-input v-model="addForm.age"></el-input>
-              </el-form-item>
-                 <el-form-item label="头像" prop="avatar">
-                <el-input v-model="addForm.avatar"></el-input>
-              </el-form-item>
-                 <el-form-item label="地址" prop="address">
-                <el-input v-model="addForm.address"></el-input>
-              </el-form-item>
-                 <el-form-item label="邮箱" prop="email">
-                <el-input v-model="addForm.email"></el-input>
-              </el-form-item>
-                 <el-form-item label="邮箱是否验证 0-否 1-是" prop="validEmail">
-                <el-input v-model="addForm.validEmail"></el-input>
-              </el-form-item>
-                 <el-form-item label="电话" prop="tel">
-                <el-input v-model="addForm.tel"></el-input>
-              </el-form-item>
-                 <el-form-item label="电话是否验证 0-否 1-是" prop="validTel">
-                <el-input v-model="addForm.validTel"></el-input>
-              </el-form-item>
-                 <el-form-item label="用户状态 0-未使用 1-正常 2-锁定 3-过期" prop="status">
-                <el-input v-model="addForm.status"></el-input>
-              </el-form-item>
-            </el-form>
-          </span>
+      <span>
+        <el-form :model="addForm" :rules="checkRules" ref="addFormRef" label-width="120px">
+          <el-form-item label="登陆账户" prop="loginCode">
+            <el-input v-model="addForm.loginCode"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="addForm.password"></el-input>
+          </el-form-item>
+          <el-form-item label="用户名称" prop="userName">
+            <el-input v-model="addForm.userName"></el-input>
+          </el-form-item>
+          <el-form-item label="性别" prop="sex">
+            <el-input v-model="addForm.sex"></el-input>
+          </el-form-item>
+          <el-form-item label="年龄" prop="age">
+            <el-input v-model="addForm.age"></el-input>
+          </el-form-item>
+          <el-form-item label="头像" prop="avatar">
+            <el-input v-model="addForm.avatar"></el-input>
+          </el-form-item>
+          <el-form-item label="地址" prop="address">
+            <el-input v-model="addForm.address"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="addForm.email"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱是否验证 0-否 1-是" prop="validEmail">
+            <el-input v-model="addForm.validEmail"></el-input>
+          </el-form-item>
+          <el-form-item label="电话" prop="tel">
+            <el-input v-model="addForm.tel"></el-input>
+          </el-form-item>
+          <el-form-item label="电话是否验证 0-否 1-是" prop="validTel">
+            <el-input v-model="addForm.validTel"></el-input>
+          </el-form-item>
+          <el-form-item label="用户状态 0-未使用 1-正常 2-锁定 3-过期" prop="status">
+            <el-input v-model="addForm.status"></el-input>
+          </el-form-item>
+        </el-form>
+      </span>
       <span slot="footer" class="dialog-footer">
-            <el-button
-              @click="visibleConfig.add = false">{{defaultSettings.cancelButtonText}}</el-button>
-            <el-button type="primary"
-                       @click="doAddSave">{{defaultSettings.confirmButtonText}}</el-button>
-          </span>
+        <el-button
+          @click="visibleConfig.add = false">{{defaultSettings.cancelButtonText}}</el-button>
+        <el-button type="primary"
+                   @click="doAddSave">{{defaultSettings.confirmButtonText}}</el-button>
+      </span>
     </el-dialog>
 
     <!-- 编辑 -->
@@ -169,42 +167,36 @@
                @close="doEditClose">
           <span>
             <el-form :model="editForm" :rules="checkRules" ref="editFormRef" label-width="120px">
-                   <el-form-item label="登陆账户" prop="loginCode">
-                 <el-input v-model="editForm.loginCode"></el-input>
-               </el-form-item>
-                   <el-form-item label="密码" prop="password">
-                 <el-input v-model="editForm.password"></el-input>
-               </el-form-item>
-                   <el-form-item label="用户名称" prop="userName">
-                 <el-input v-model="editForm.userName"></el-input>
-               </el-form-item>
-                   <el-form-item label="性别 1-男 2-女" prop="sex">
-                 <el-input v-model="editForm.sex"></el-input>
-               </el-form-item>
-                   <el-form-item label="年龄" prop="age">
-                 <el-input v-model="editForm.age"></el-input>
-               </el-form-item>
-                   <el-form-item label="头像" prop="avatar">
-                 <el-input v-model="editForm.avatar"></el-input>
-               </el-form-item>
-                   <el-form-item label="地址" prop="address">
-                 <el-input v-model="editForm.address"></el-input>
-               </el-form-item>
-                   <el-form-item label="邮箱" prop="email">
-                 <el-input v-model="editForm.email"></el-input>
-               </el-form-item>
-                   <el-form-item label="邮箱是否验证 0-否 1-是" prop="validEmail">
-                 <el-input v-model="editForm.validEmail"></el-input>
-               </el-form-item>
-                   <el-form-item label="电话" prop="tel">
-                 <el-input v-model="editForm.tel"></el-input>
-               </el-form-item>
-                   <el-form-item label="电话是否验证 0-否 1-是" prop="validTel">
-                 <el-input v-model="editForm.validTel"></el-input>
-               </el-form-item>
-                   <el-form-item label="用户状态 0-未使用 1-正常 2-锁定 3-过期" prop="status">
-                 <el-input v-model="editForm.status"></el-input>
-               </el-form-item>
+              <el-form-item label="登陆账户" prop="loginCode">
+               <el-input v-model="editForm.loginCode"></el-input>
+              </el-form-item>
+              <el-form-item label="密码" prop="password">
+                <el-input v-model="editForm.password"></el-input>
+              </el-form-item>
+              <el-form-item label="用户名称" prop="userName">
+                <el-input v-model="editForm.userName"></el-input>
+              </el-form-item>
+              <el-form-item label="性别" prop="sex">
+                <el-input v-model="editForm.sex"></el-input>
+              </el-form-item>
+              <el-form-item label="年龄" prop="age">
+                <el-input v-model="editForm.age"></el-input>
+              </el-form-item>
+              <el-form-item label="头像" prop="avatar">
+                <el-input v-model="editForm.avatar"></el-input>
+              </el-form-item>
+              <el-form-item label="地址" prop="address">
+                <el-input v-model="editForm.address"></el-input>
+              </el-form-item>
+              <el-form-item label="邮箱" prop="email">
+                <el-input v-model="editForm.email"></el-input>
+              </el-form-item>
+              <el-form-item label="电话" prop="tel">
+                <el-input v-model="editForm.tel"></el-input>
+              </el-form-item>
+              <el-form-item label="用户状态" prop="status">
+                <el-input v-model="editForm.status"></el-input>
+              </el-form-item>
             </el-form>
           </span>
       <span slot="footer" class="dialog-footer">
@@ -230,8 +222,7 @@
 
         // 查询参数对象
         searchForm: {
-          current: 1,
-          id: ''
+          current: 1
         },
         // 列表数据
         gridData: {
