bb663cdc806f8200134299e20f0b218008275fd6
zzw <errmao@aliyun.com>
2020-08-13 14:53:45 +0800

    工作流整合

--- tyrande-flow/src/main/java/org/tyrande/flow/common/config/FlowableConfig.java
+++ tyrande-flow/src/main/java/org/tyrande/flow/common/config/FlowableConfig.java
@@ -1,9 +1,14 @@
 package org.tyrande.flow.common.config;
 
 import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.TaskService;
 import org.flowable.spring.SpringProcessEngineConfiguration;
 import org.flowable.spring.boot.EngineConfigurationConfigurer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 
@@ -14,6 +19,7 @@ import javax.annotation.Resource;
  *
  * @author Tyrande
  */
+@Slf4j
 @Configuration
 public class FlowableConfig implements EngineConfigurationConfigurer<SpringProcessEngineConfiguration> {
 
@@ -23,11 +29,26 @@ public class FlowableConfig implements EngineConfigurationConfigurer<SpringProce
     @Value("${flowable.data-source}")
     private String dataSource;
 
+    /**
+     * 引擎高级配置
+     */
     @Override
     public void configure(SpringProcessEngineConfiguration springProcessEngineConfiguration) {
         // 设置指定的数据源
         if (!StringUtils.isEmpty(dataSource)) {
             springProcessEngineConfiguration.setDataSource(dynamicRoutingDataSource.getDataSource("flow"));
         }
+    }
+
+    /**
+     * 启动任务
+     */
+    @Bean
+    public CommandLineRunner init(final RepositoryService repositoryService,
+                                  final TaskService taskService) {
+        return strings -> {
+            log.info("[流程定义数量]：{} 个", repositoryService.createProcessDefinitionQuery().count());
+            log.info("[流程任务数量]：{} 个", taskService.createTaskQuery().count());
+        };
     }
 }
---
+++ tyrande-flow/src/main/java/org/tyrande/flow/service/FlowableService.java
@@ -0,0 +1,9 @@
+package org.tyrande.flow.service;
+
+/**
+ * 工作流通用服务接口
+ *
+ * @author Tyrande
+ */
+public interface FlowableService {
+}
