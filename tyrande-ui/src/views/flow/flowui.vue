d65c40394dc0924305f793d3882114f48cf2bd62
zzw <errmao@aliyun.com>
2020-08-19 13:41:25 +0800

    最大化 引入flowable设计器

--- tyrande-ui/src/views/flow/flowui.vue
+++ tyrande-ui/src/views/flow/flowui.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="iframe-container">
-    <iframe :src="src" scrolling="auto" frameborder="0" class="frame">
-    </iframe>
+  <div>
+    <iframe id="show-iframe" :src="src" scrolling="auto" frameborder="0" class="frame" />
   </div>
 </template>
 
@@ -15,6 +14,11 @@ export default {
   },
   mounted() {
     this.src = process.env['VUE_APP_HOST_URL'] + '/tyrande/flowui/index.html'
+    const oIframe = document.getElementById('show-iframe')
+    const deviceWidth = document.documentElement.clientWidth
+    const deviceHeight = document.documentElement.clientHeight
+    oIframe.style.width = deviceWidth + 'px'
+    oIframe.style.height = deviceHeight - 60 + 'px'
   },
   methods: {
   }
